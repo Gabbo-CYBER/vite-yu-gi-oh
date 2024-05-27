72e205e1914cc5b4d202d090efc4e1f9c4d99aa5
Your Name <you@example.com>
2024-05-27 19:57:16 +0200

    1 m-1 commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+    
+    
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+   
+    }
+  }
+ 
+</script>
+
+<template>
+  <AppHeader>
+     <!-- <a href="google.it">Link non stilizzato in header</a>  -->
+  </AppHeader>
+
+  <AppMain>
+
+
+  </AppMain>
+  <AppFooter>
+
+
+
+
+  </AppFooter>
+
+
+    
+
+      
+</template>
+
+<style scoped>
+
+</style>
+
---
+++ src/components/AppMain.vue
@@ -0,0 +1,28 @@
+<script>
+
+export default {
+    name: "AppFooter",
+    data() {
+        return {
+             // imgVitePublic: "/vite.svg",
+            // imgVueAssets: "vue.svg"
+
+        }
+    }
+}
+
+
+    // methods: {
+    //     getImage(nomefile) {
+    //         return new URL(`../assets/${nomefile}`, import.meta.url);
+    //     }
+    // }
+
+
+
+</script>
+
+
+<template>
+    
+</template>
